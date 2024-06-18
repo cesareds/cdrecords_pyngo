ff427ae93cf6309418d98dc5db1cce2d86c290d7
cesardesouza <cesarsouza.2002@icloud.com>
2024-06-18 11:03:50 -0300

    fixing reddirects

--- app.py
+++ app.py
@@ -169,4 +169,4 @@ def submit_musica():
         return f"Erro ao salvar música: {e}", 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True) #se a porta 8080 estiver ocupada, botar outra
