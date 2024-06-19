cce0bd40f95beadcd5f6963a920a6febe573a9e3
cesardesouza <cesarsouza.2002@icloud.com>
2024-06-19 08:16:57 -0300

    la ele

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def index():
 def show_musicos():
     try:
         musicos = list(collection_musico.find())
-        return render_template('musicos.html', musicos=musicos)
+        return render_template('musicos.html', data=musicos)
     except Exception as e:
         return f"Erro ao recuperar músicos: {e}", 500
 
@@ -70,7 +70,7 @@ def submit_musico():
 def show_bandas():
     try:
         bandas = list(collection_banda.find())
-        return render_template('bandas.html', bandas=bandas)
+        return render_template('bandas.html', data=bandas)
     except Exception as e:
         return f"Erro ao recuperar bandas: {e}", 500
 
@@ -103,7 +103,7 @@ def submit_banda():
 def show_discos():
     try:
         discos = list(collection_disco.find())
-        return render_template('discos.html', discos=discos)
+        return render_template('discos.html', data=discos)
     except Exception as e:
         return f"Erro ao recuperar discos: {e}", 500
 
@@ -142,7 +142,7 @@ def submit_disco():
 def show_musicas():
     try:
         musicas = list(collection_musica.find())
-        return render_template('musicas.html', musicas=musicas)
+        return render_template('musicas.html', data=musicas)
     except Exception as e:
         return f"Erro ao recuperar músicas: {e}", 500
 
@@ -168,26 +168,62 @@ def submit_musica():
     except Exception as e:
         print(f"Erro ao salvar música: {e}")
         return f"Erro ao salvar música: {e}", 500
+    
+@app.route('/search', methods=['POST'])
+def show_sorted():
+    types = ['bandas', 'discos', 'musicas', 'musicos', 'instrumentos']
+    template = ''
+    sort = ''
+    for t in types:
+        try:
+            sort = request.form[f'sort_{t}']
+            print(sort)
+            template = f'{t}.html'
+            break
+        except KeyError as e:
+            oi = 1
+
+    try:
+        if t == 'bandas':
+            data = list(collection_banda.find({ 'nome': { '$regex': sort, '$options': 'i' } }).sort('nome', 1))  # Ordena por nome ascendente
+        elif t == 'musicos':
+            data = list(collection_musico.find({ 'nome': { '$regex': sort, '$options': 'i' } }).sort('nome', 1))  # Ordena por nome ascendente
+        elif t == 'discos':
+            data = list(collection_disco.find({ 'titulo': { '$regex': sort, '$options': 'i' } }).sort('nome', 1))  # Ordena por nome ascendente
+        elif t == 'musicas':
+            data = list(collection_musica.find({ 'titulo': { '$regex': sort, '$options': 'i' } }).sort('nome', 1))  # Ordena por nome ascendente
+        elif t == 'instrumentos':
+            data = list(collection_instrumento.find({ 'nome': { '$regex': sort, '$options': 'i' } }).sort('nome', 1))  # Ordena por nome ascendente
+        return render_template(template, data=data)
+    except Exception as e:
+        return f"Erro ao recuperar músicos: {e}", 500
 
 
 @app.route('/instrumentos')
 def show_instrumentos():
     try:
         instrumentos = list(collection_instrumento.find())
-        return render_template('instrumentos.html', instrumentos=instrumentos)
+        return render_template('instrumentos.html', data=instrumentos)
     except Exception as e:
         return f"Erro ao recuperar instrumentos: {e}", 500
     
 @app.route('/submit_instrumento', methods=['POST'])
 def submit_instrumento():
-    instrumento = {
-        'marca': request.form['marca'],
-        'tipo': request.form['tipo'],
-        'nome': request.form['nome'],
-        'url': request.form['url']
+    print(request.form)
+    nome = request.form['nome']
+    descricao = request.form['descricao']
+    query = {
+        'nome': nome,
+        'descricao': descricao
     }
-    collection_instrumento.append(instrumento)
-    return redirect(url_for('show_instrumentos'))
+    try:
+        print("Salvando...")
+        collection_instrumento.insert_one(query)
+        return redirect(url_for('show_instrumentos'))
+    except Exception as e:
+        print(f"Erro ao salvar instrumento: {e}")
+        return f"Erro ao salvar instrumento: {e}", 500
+    
 
 
 if __name__ == '__main__':
